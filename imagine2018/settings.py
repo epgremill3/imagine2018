a46b83c7226657664d98688c352814b3f2a41c8e
Ernie Gremillion <epgremill3@yahoo.com>
2018-09-29 13:07:11 -0400

    fix copy/paste bug in settings

--- imagine2018/settings.py
+++ imagine2018/settings.py
@@ -78,7 +78,7 @@ if 'RDS_HOSTNAME' in os.environ:
     # n.b. assumes that if os.environ['RDS_HOSTNAME'] is present all others are too...
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
