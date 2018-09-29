787483aeaad793fc06cb156089b19c7fdd41b546
alanthejudoka <alasal21@bergen.org>
2018-09-29 17:13:24 -0400

    not done yet

---
+++ app/Delete Vehicle.py
@@ -0,0 +1,25 @@
+# add names, delete a name
+vehicles = []
+
+def delete_vehicle(vehicle):
+
+
+def main():
+    a = ("Vehicle A")
+    b = ("Vehicle B")
+    c = ("Vehicle C")
+    d = ("Vehicle D")
+    e = ("Vehicle E")
+    f = ("Vehicle F")
+
+    vehicles.append(a)
+    vehicles.append(b)
+    vehicles.append(c)
+    vehicles.append(d)
+    vehicles.append(e)
+    vehicles.append(f)
+
+    delete_vehicle(c)
+
+
+main()
